1b7871920e5b69b5d100bb6dcbe08ace16037802
aditya61006 <adityachoudhary61006@gmail.com>
2026-02-16 00:54:34 +0530

    port error solving

--- SERVER/server.js
+++ SERVER/server.js
@@ -52,6 +52,8 @@ app.use("/api/messages", messageRouter);
 //  Database connection
 await connectDB();
 
-const PORT = process.env.PORT|| 5000;
 
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT|| 5000;
 server.listen(PORT,()=>console.log("Server is running on Port: "+PORT));
+}
